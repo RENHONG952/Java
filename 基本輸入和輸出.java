f42180f5ac562e8fa0db8ba9d3cfc02048b525e6
RENHONG <bear97523@gmail.com>
2025-07-09 20:33:28 +0800

    Add files via upload

---
+++ 基本輸入和輸出.java
@@ -0,0 +1,20 @@
+package Java;
+
+// 輸入類別
+import java.util.Scanner; 
+
+public class 基本輸入和輸出 {
+    public static void main(String[] args) {
+
+        // 輸入內容
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入名稱 : ");
+        String name = scanner.nextLine();
+
+        // 輸出結果
+        System.out.println("Hello ~ " + name + "！");
+
+        // 釋放資源
+        scanner.close();
+    }
+}
